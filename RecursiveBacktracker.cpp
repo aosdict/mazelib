8defe52f8ad5843f5ebd6af3f797317b6c2d0580
aosdict <aosdict@gmail.com>
2016-04-22 19:16:28 -0400

    Changed algorithm details format for Recursive Backtracker

--- RecursiveBacktracker.cpp
+++ RecursiveBacktracker.cpp
@@ -2,15 +2,26 @@
 #include "MazeSpace.h"
 #include <iostream>
 
+/* Behavior regarding structures:
+   If the space being passed over or the space being passed into is a
+   structure wall, do not move there or recurse.
+   If the space being passed over is a normal path, do not move there or
+   recurse.
+   If the space being passed into is any path and a loop_prob check passes,
+   convert the space being passed over into a normal path ONLY IF it is a normal
+   wall, and do not move there or recurse.
+   */
+
+
 /* All possible permutations of the bits 00, 01, 10, and 11
-   Each value corresponds to a direction.
-   This is the most efficient way to generate a random ordering of the four
-   directions, by simply selecting a number at random from this array. */
+  Each value corresponds to a direction.
+  This is the most efficient way to generate a random ordering of the four
+  directions, by simply selecting a number at random from this array. */
 static const unsigned char bitperms[24] = {
-  0x1B, 0x1E, 0x27, 0x2D, 0x36, 0x39,
-  0x4B, 0x4E, 0x63, 0x6C, 0x72, 0x78,
-  0x87, 0x8D, 0x93, 0x9C, 0xB1, 0xB4,
-  0xC6, 0xC9, 0xD2, 0xD8, 0xE1, 0xE4
+ 0x1B, 0x1E, 0x27, 0x2D, 0x36, 0x39,
+ 0x4B, 0x4E, 0x63, 0x6C, 0x72, 0x78,
+ 0x87, 0x8D, 0x93, 0x9C, 0xB1, 0xB4,
+ 0xC6, 0xC9, 0xD2, 0xD8, 0xE1, 0xE4
 };
 
 namespace Maze {
